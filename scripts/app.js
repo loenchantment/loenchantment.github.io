f33b2aceb00509d5e09d97f0997f14febaad70ec
loenchantment <jac17040@byui.edu>
2023-01-05 21:50:11 -0600

    Jan 5th last

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,4 @@ const copyright = document.querySelector('#copyright');
 h1.textContent = 'Sister Becky Jacobs, WDD 230 Student';
 
 
-copyright.textContent = '© ${new Date().getFullYear()}';
+copyright.textContent = '© 2023';
