56d8dd9cfac1be4d449db763a286424f780f8860
loenchantment <jac17040@byui.edu>
2023-01-06 16:16:19 -0600

    week01

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 const h1 = document.querySelector('h1');
 const copyright = document.querySelector('#copyright');
 
-h1.textContent = 'Sister Becky Jacobs, WDD 230 Student';
+h1.textContent = 'Becky Jacobs, WDD 230 Assignments';
 
 const currentYear = new Date().getFullYear();
 copyright.textContent = `© ${new Date().getFullYear()}`;
+
+//this gets the value input in the text box
+let quantity = document.querySelector(`#q`).value;
