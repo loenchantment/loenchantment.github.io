372480e7420a91a0df3e2cc33c6e2228dabcd4e0
loenchantment <jac17040@byui.edu>
2023-01-07 13:42:39 -0600

    final week 01

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,13 @@
 const h1 = document.querySelector('h1');
 const copyright = document.querySelector('#copyright');
 
-h1.textContent = 'Becky Jacobs, WDD 230 Assignments';
+h1.textContent = 'Becky Jacobs: Web Design Student';
 
 const currentYear = new Date().getFullYear();
 copyright.textContent = `© ${new Date().getFullYear()}`;
 
 //this gets the value input in the text box
-let quantity = document.querySelector(`#q`).value;
+//let quantity = document.querySelector(`#q`).value;
+
+let text = document.lastModified;
+document.getElementById("datemod").innerHTML = text;
