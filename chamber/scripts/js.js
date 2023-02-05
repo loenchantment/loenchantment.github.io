7f8444208ead42b121d47f6dcfffff0abde69dbe
loenchantment <jac17040@byui.edu>
2023-02-04 21:49:42 -0600

    Feb 4 craziness

--- chamber/scripts/js.js
+++ chamber/scripts/js.js
@@ -20,3 +20,16 @@ const hambutton = document.querySelector('#menu');
 // Add a simple arrow function that listens for the <li> hamburger button click event.
 // When clicked, the <ul class="navigation">'s class list toggle'
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+/*I tried working on this for longer than I care to admit :(
+
+const banner = document.querySelector(".banner");
+const today = new Date().getDay();
+
+if (today = 6) {
+    banner.style = display: block;
+}
+  */  
+
+
+ 
