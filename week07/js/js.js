14c1c7b93bfd561d0ceac94befd47d6a636e615e
loenchantment <jac17040@byui.edu>
2023-02-16 12:10:30 -0600

    Feb 16, 2023

---
+++ week07/js/js.js
@@ -0,0 +1,42 @@
+/*const currentYear = new Date().getFullYear();*/
+copyright.textContent = `© ${new Date().getFullYear()}`;
+
+//this gets the value input in the text box
+//let quantity = document.querySelector(`#q`).value;
+
+let text = document.lastModified;
+document.getElementById("datemod").innerHTML = text;
+
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+
+  if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
+  
