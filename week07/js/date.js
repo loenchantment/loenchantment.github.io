b572f5645aca493322f27a8c3544c433f1d110b9
loenchantment <jac17040@byui.edu>
2023-02-23 23:35:55 -0600

    Feb 23, 2023

---
+++ week07/js/date.js
@@ -0,0 +1,8 @@
+/*const currentYear = new Date().getFullYear();*/
+copyright.textContent = `© ${new Date().getFullYear()}`;
+
+//this gets the value input in the text box
+//let quantity = document.querySelector(`#q`).value;
+
+let text = document.lastModified;
+document.getElementById("datemod").innerHTML = text;
--- week07/js/js.js
+++ week07/js/js.js
@@ -1,40 +1,7 @@
-/*const currentYear = new Date().getFullYear();*/
-copyright.textContent = `© ${new Date().getFullYear()}`;
 
-//this gets the value input in the text box
-//let quantity = document.querySelector(`#q`).value;
 
-let text = document.lastModified;
-document.getElementById("datemod").innerHTML = text;
 
-/*
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
-};
 
-if('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if(item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-	
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-}
- */ 
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
   image.setAttribute("src", image.getAttribute("data-src"));
@@ -42,6 +9,8 @@ const loadImages = (image) => {
     image.removeAttribute("data-src");
   };
 };
+
+
 
 
 if ("IntersectionObserver" in window) {
@@ -63,8 +32,3 @@ if ("IntersectionObserver" in window) {
 }
 
 
-
-
-
-
- 
