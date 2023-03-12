38f487c370f3826be0fbc81fb5e074d0f2e1ad03
loenchantment <jac17040@byui.edu>
2023-03-12 00:28:03 -0600

    March 11, 2023last

--- chamber/scripts/js.js
+++ chamber/scripts/js.js
@@ -1,20 +1,21 @@
+//this is for the date at the top of the html page
 const datefield = document.querySelector(".date");
 
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(
-	now
-);
+
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
-/*const currentYear = new Date().getFullYear();*/
+
+//this puts the date in the copyriight in the footer 
 copyright.textContent = `© ${new Date().getFullYear()}`;
 
+//this puts the last modified date in the footer
 let text = document.lastModified;
 document.getElementById("datemod").innerHTML = text;
 
 /*banner script*/
-
 const banner = document.querySelector(".banner");
 
 if (now.getDay() == 1 || now.getDay() == 2)  {
@@ -22,12 +23,12 @@ if (now.getDay() == 1 || now.getDay() == 2)  {
 }
   
 
-// Store the selected elements that we are going to use. 
+//Connecting the elements to the html. 
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
 // Add a simple arrow function that listens for the <li> hamburger button click event.
-// When clicked, the <ul class="navigation">'s class list toggle'
+// When clicked, the <ul class="navigation">'s class list toggle or switches to responsive
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 
